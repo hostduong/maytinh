8d3ed6815d5e9b5d43941d87df425bbffd6a2b8f
hostduong <hostduong@gmail.com>
2026-02-04 16:07:08 +0700

    Update chi_muc.go

--- mo_hinh/chi_muc.go
+++ mo_hinh/chi_muc.go
@@ -1,7 +1,7 @@
 package mo_hinh
 
-// DongBatDauDuLieu : Dữ liệu bắt đầu từ dòng 11 trong Excel => Index = 10
-const DongBatDauDuLieu = 10
+// DongBatDauDuLieu : Dữ liệu bắt đầu từ dòng 11 trong Excel
+const DongBatDauDuLieu = 11
 
 // 1. SAN_PHAM (A-S)
 const (

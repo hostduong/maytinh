d4e345fe48182367b4dcecc0659df98dc43a82d9
hostduong <hostduong@gmail.com>
2026-02-05 19:21:06 +0700

    Update cai_dat.go

--- cau_hinh/cai_dat.go
+++ cau_hinh/cai_dat.go
@@ -14,11 +14,13 @@ type CauHinhHeThong struct {
 var BienCauHinh CauHinhHeThong
 
 func KhoiTaoCauHinh() {
+	// Lấy ID Sheet
 	idSheet := os.Getenv("SHEET_ID")
 	if idSheet == "" {
 		idSheet = "17f5js4C9rY7GPd4TOyBidkUPw3vCC6qv6y8KlF3vNs8"
 	}
 
+	// Lấy Port
 	congWeb := os.Getenv("PORT")
 	if congWeb == "" {
 		congWeb = "8080"
@@ -29,5 +31,5 @@ func KhoiTaoCauHinh() {
 		CongChayWeb: congWeb,
 	}
 
-	log.Println("--- Cấu hình đã tải xong ---")
+	log.Println("--- [CẤU HÌNH] Đã tải xong (Chế độ Public) ---")
 }

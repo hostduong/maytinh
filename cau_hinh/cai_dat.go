b1fd45660b2508090e2247cc500885afcb90f515
hostduong <hostduong@gmail.com>
2026-02-05 19:42:51 +0700

    Update cai_dat.go

--- cau_hinh/cai_dat.go
+++ cau_hinh/cai_dat.go
@@ -6,30 +6,33 @@ import (
 )
 
 type CauHinhHeThong struct {
-	IdFileSheet string
-	CongChayWeb string
-	// Đã xóa FileChungThuc
+	IdFileSheet string // ID của file Google Sheet
+	CongChayWeb string // Port (VD: 8080)
+	// [ĐÃ XÓA] FileChungThuc -> Để không gây lỗi tìm file
 }
 
 var BienCauHinh CauHinhHeThong
 
 func KhoiTaoCauHinh() {
-	// Lấy ID Sheet
+	// 1. Lấy ID Sheet
 	idSheet := os.Getenv("SHEET_ID")
 	if idSheet == "" {
+		// ID mặc định của bạn
 		idSheet = "17f5js4C9rY7GPd4TOyBidkUPw3vCC6qv6y8KlF3vNs8"
 	}
 
-	// Lấy Port
+	// 2. Lấy Port
 	congWeb := os.Getenv("PORT")
 	if congWeb == "" {
 		congWeb = "8080"
 	}
 
+	// 3. Gán vào biến
 	BienCauHinh = CauHinhHeThong{
 		IdFileSheet: idSheet,
 		CongChayWeb: congWeb,
 	}
 
-	log.Println("--- [CẤU HÌNH] Đã tải xong (Chế độ Public) ---")
+	log.Println("--- [CẤU HÌNH] Đã tải xong cài đặt (Public Mode) ---")
+	log.Println("--- ID Sheet:", BienCauHinh.IdFileSheet)
 }
